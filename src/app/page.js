5c8ae2c314d665be325ccf7fa08e177762bb1d06
zshadabm@gmail.com <zshadabm@gmail.com>
2024-06-08 20:03:07 +0300

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -106,7 +106,7 @@ export default function Home() {
         <a href="#">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Elegant Drama (IRE) </h5>
         </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">2021 Produce: Elegant Call (IRE) (f. by Soldier's Call (GB)), placed twice at 2 and 3 years, 2024.</p>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">2021 Produce: Elegant Call (IRE) (f. by Soldiers Call (GB)), placed twice at 2 and 3 years, 2024.</p>
         <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Read more
              <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
